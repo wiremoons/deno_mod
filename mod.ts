bd4e11a2238a96e94d85b049783a075d006afa04
Simon Rowe <simon@wiremoons.com>
2021-11-28 09:26:10 +0000

    List exported functions - instead of using '*' for each module file

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@
  * @brief A collection of modules written in TypeScript for common Deno programming tasks.
  *
  * @link https://deno.land/x/deno_mod/mod.ts
- * @link https://raw.githubusercontent.com/wiremoons/deno_mod/0.7.1/mod.ts
+ * @link https://raw.githubusercontent.com/wiremoons/deno_mod/mod.ts
  *
  * @author     simon rowe <simon@wiremoons.com>
  * @license    open-source released under "MIT License"
@@ -14,8 +14,10 @@
  *
  * @details See each included module listed below for specific purpose and usage details.
  */
-export * from "./cli_version.ts";
-export * from "./exists_dir.ts";
-export * from "./exists_file.ts";
-export * from "./to_title_case.ts";
-export * from "./type_guard.ts";
+import {toTitleCaseFirst} from "./to_title_case.ts";
+
+export { cliVersion } from "./cli_version.ts";
+export { existsDir } from "./exists_dir.ts";
+export { existsFile } from "./exists_file.ts";
+export { toTitleCaseFirst } from "./to_title_case.ts";
+export { isNumber, isString } from "./type_guard.ts";
